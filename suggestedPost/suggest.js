f1f75351225db1d2ad847b3c139e1da7d09a22ba
Andrew <a@ndrewsemler.com>
2016-03-08 19:44:28 +1100

    initial commit

---
+++ suggestedPost/suggest.js
@@ -0,0 +1,16 @@
+//register for Dom Events
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var observer = new MutationObserver(function(mutations, observer) {
+    remove ();
+});
+observer.observe(document, {subtree: true, attributes: true,childList: true});
+
+function remove () {
+  $("._5g-l").each (function (i) {
+    if ($($(this).children()[0]).text() == "Suggested Post") {
+      $(this).parent().parent().parent().parent().parent().remove();
+    }
+  });
+}
+
+remove();
